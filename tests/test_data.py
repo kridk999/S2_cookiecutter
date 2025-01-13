a074cd7b939cf01439b4f3bc27c5fcadf0f1ad48
kridk999 <17kkjaer@gmail.com>
2025-01-13 17:13:32 +0100

    pre-commit

--- tests/test_data.py
+++ tests/test_data.py
@@ -5,18 +5,18 @@ from src.mnist_cookie.data import corrupt_mnist
 
 def test_data():
     train, test = corrupt_mnist()
-    assert len(train) == 30000,  "Dataset did not have the correct number of samples" 
+    assert len(train) == 30000,  "Dataset did not have the correct number of samples"
     assert len(test) == 5000, "Dataset did not have the correct number of samples"
     for data in train:
         assert data[0].shape == (1, 28, 28)
     for data in test:
         assert data[0].shape == (1, 28, 28)
-    
+
     train_targets = train.tensors[1].unique()
     test_targets = test.tensors[1].unique()
-    
-    assert (train_targets == torch.arange(0,10)).all() 
+
+    assert (train_targets == torch.arange(0,10)).all()
     assert (test_targets == torch.arange(0,10)).all()
-        
+
 
 #testing github pull request setup
